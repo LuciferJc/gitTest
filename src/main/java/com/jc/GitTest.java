eaadd7090ca0dd26174e37148bb7775d0773a0e3
Lucifer <jc13694658167@163.com>
2021-12-18 18:20:01 +0800

    hotfix提交1

--- src/main/java/com/jc/GitTest.java
+++ src/main/java/com/jc/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("第四次提交");
+        System.out.println("hotfix提交");
     }
 }
