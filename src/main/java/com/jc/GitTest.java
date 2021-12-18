a423c15f82fd5c495a884763323cc987003154f8
Lucifer <jc13694658167@163.com>
2021-12-18 18:04:38 +0800

    提交信息3

--- src/main/java/com/jc/GitTest.java
+++ src/main/java/com/jc/GitTest.java
@@ -7,6 +7,6 @@ package com.jc;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
-        System.out.println("第二次提交");
+        System.out.println("第三次提交");
     }
 }
