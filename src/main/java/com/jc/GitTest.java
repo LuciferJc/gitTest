793c7f3e02ee86cbbe8a89666ef54274576c0dee
Lucifer <jc13694658167@163.com>
2021-12-18 17:59:57 +0800

    提交信息1

---
+++ src/main/java/com/jc/GitTest.java
@@ -0,0 +1,11 @@
+package com.jc;
+
+/**
+ * @author Lucifer
+ * @create 2021-12-18 下午 5:15
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
